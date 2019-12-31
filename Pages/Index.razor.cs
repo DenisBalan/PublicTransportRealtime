c168da57f0b34ed92293834c1d27a53f0747f77b
Denis Balan <33955091+DenisBalan@users.noreply.github.com>
2019-12-31 10:34:30 +0200

    Add Index code-behid page

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+
+namespace PublicTransportRealtime.Pages
+{
+    public class IndexModel: ComponentBase, IDisposable
+    {
+        [Inject] JSRuntime _clientRuntime { get; set; }
+
+        public void Dispose()
+        {
+        }
+    }
+}
