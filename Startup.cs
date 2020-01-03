70aec5fde54f2ad15b08093b9222f13293852e5d
Denis Balan <33955091+DenisBalan@users.noreply.github.com>
2020-01-03 17:07:39 +0200

    Add modal from Blazored.Modal

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blazored.Modal;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -34,6 +35,8 @@ namespace PublicTransportRealtime
             services.Configure<EndpointConfiguration>(
                 Configuration.GetSection(nameof(EndpointConfiguration))
                 );
+
+            services.AddBlazoredModal();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
